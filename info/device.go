dbf8324348e50cbeb7cc27511b8378d7e0ec2eca
Redmomn <109732988+Redmomn@users.noreply.github.com>
2024-04-07 15:52:55 +0800

    fix: rand system info

--- info/device.go
+++ info/device.go
@@ -19,10 +19,9 @@ type DeviceInfo struct {
 
 func NewDeviceInfo(uin int) *DeviceInfo {
 	return &DeviceInfo{
-		Guid:       strconv.Itoa(uin),
-		DeviceName: fmt.Sprintf("Lagrange-%X", utils.Md5Digest([]byte(strconv.Itoa(uin)))[0:4]),
-		// TODO 这里好像不能获取os的KernelVersion,使用go arch罢
-		SystemKernel:  fmt.Sprintf("%s %s", runtime.GOOS, platform.GetVersion()),
+		Guid:          strconv.Itoa(uin),
+		DeviceName:    fmt.Sprintf("Lagrange-%X", utils.Md5Digest([]byte(strconv.Itoa(uin)))[0:4]),
+		SystemKernel:  fmt.Sprintf("%s %s", platform.GetSystem(), platform.GetVersion()),
 		KernelVersion: runtime.GOARCH,
 	}
 }
