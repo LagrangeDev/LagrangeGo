6ca71c9309dc9311b73df0d1813a98111397b6c1
Linwenxuan <llwwxxllwwxx@gmail.com>
2024-04-10 11:09:09 +0800

    fix: length of Guid

--- info/device.go
+++ info/device.go
@@ -18,7 +18,7 @@ type DeviceInfo struct {
 
 func NewDeviceInfo(uin int) *DeviceInfo {
 	return &DeviceInfo{
-		Guid:          strconv.Itoa(uin),
+		Guid:          fmt.Sprintf("%X", utils.Md5Digest([]byte(strconv.Itoa(uin)))),
 		DeviceName:    fmt.Sprintf("Lagrange-%X", utils.Md5Digest([]byte(strconv.Itoa(uin)))[0:4]),
 		SystemKernel:  fmt.Sprintf("%s %s", platform.GetSystem(), platform.GetVersion()),
 		KernelVersion: platform.GetVersion(),
