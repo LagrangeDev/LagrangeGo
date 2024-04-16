d79a846c0a8134bacfa98243fb489bd81a53f2b7
Redmomn <109732988+Redmomn@users.noreply.github.com>
2024-04-16 22:42:19 +0800

    feat: cache

---
+++ cache/base.go
@@ -0,0 +1,25 @@
+package cache
+
+import (
+	"sync"
+
+	"github.com/LagrangeDev/LagrangeGo/entity"
+)
+
+type Cache struct {
+	refreshLock sync.RWMutex
+	// FriendCache 好友缓存 uin *entity.Friend
+	FriendCache map[uint32]*entity.Friend
+	// GroupCache 群信息缓存 groupUin *entity.Group
+	GroupCache map[uint32]*entity.Group
+	// GroupMemberCache 群内群员信息缓存 groupUin uin *entity.GroupMember
+	GroupMemberCache map[uint32]map[uint32]*entity.GroupMember
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		FriendCache:      make(map[uint32]*entity.Friend),
+		GroupCache:       make(map[uint32]*entity.Group),
+		GroupMemberCache: make(map[uint32]map[uint32]*entity.GroupMember),
+	}
+}
---
+++ cache/operation.go
@@ -0,0 +1,62 @@
+package cache
+
+import (
+	"github.com/LagrangeDev/LagrangeGo/entity"
+)
+
+func (c *Cache) RefreshAll(friendCache map[uint32]*entity.Friend, groupCache map[uint32]*entity.Group, groupMemberCache map[uint32]map[uint32]*entity.GroupMember) {
+	c.refreshLock.Lock()
+	defer c.refreshLock.Unlock()
+	c.FriendCache = friendCache
+	c.GroupMemberCache = groupMemberCache
+	c.GroupCache = groupCache
+}
+
+// RefreshAllFriend 刷新所有好友缓存
+func (c *Cache) RefreshAllFriend(friendCache map[uint32]*entity.Friend) {
+	c.refreshLock.Lock()
+	defer c.refreshLock.Unlock()
+	c.FriendCache = friendCache
+}
+
+// RefreshAllGroup 刷新所有群的群消息缓存
+func (c *Cache) RefreshAllGroup(groupCache map[uint32]*entity.Group) {
+	c.refreshLock.Lock()
+	defer c.refreshLock.Unlock()
+	c.GroupCache = groupCache
+}
+
+// RefreshAllGroupMember 刷新所有群的群员缓存
+func (c *Cache) RefreshAllGroupMember(groupMemberCache map[uint32]map[uint32]*entity.GroupMember) {
+	c.refreshLock.Lock()
+	defer c.refreshLock.Unlock()
+	c.GroupMemberCache = groupMemberCache
+}
+
+// RefreshFriend 刷新一个好友的缓存
+func (c *Cache) RefreshFriend(friend *entity.Friend) {
+	c.refreshLock.Lock()
+	defer c.refreshLock.Unlock()
+	c.FriendCache[friend.Uin] = friend
+}
+
+// RefreshGroup 刷新一个群的群信息缓存
+func (c *Cache) RefreshGroup(group *entity.Group) {
+	c.refreshLock.Lock()
+	defer c.refreshLock.Unlock()
+	c.GroupCache[group.GroupUin] = group
+}
+
+// RefreshGroupMember 刷新指定群的一个群成员缓存
+func (c *Cache) RefreshGroupMember(groupUin uint32, groupMember *entity.GroupMember) {
+	c.refreshLock.Lock()
+	defer c.refreshLock.Unlock()
+	c.GroupMemberCache[groupUin][groupMember.Uin] = groupMember
+}
+
+// RefreshGroupMembers 刷新一个群内的所有群成员缓存
+func (c *Cache) RefreshGroupMembers(groupUin uint32, groupMembers map[uint32]*entity.GroupMember) {
+	c.refreshLock.Lock()
+	defer c.refreshLock.Unlock()
+	c.GroupMemberCache[groupUin] = groupMembers
+}
