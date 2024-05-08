25e2846d0dfa1d36b70a1e6655924d24d416406b
源文雨 <41315874+fumiama@users.noreply.github.com>
2024-05-08 15:08:07 +0900

    optimize(packet): highway code style

--- packets/highway/HighWayUrl.go
+++ packets/highway/HighWayUrl.go
@@ -9,14 +9,13 @@ import (
 )
 
 func BuildHighWayUrlReq(tgt []byte) ([]byte, error) {
-	tgtHex := hex.EncodeToString(tgt)
-	body := &action.HttpConn0X6Ff_501{
+	return proto.Marshal(&action.HttpConn0X6Ff_501{
 		HttpConn: &action.HttpConn{
 			Field1:       0,
 			Field2:       0,
 			Field3:       16,
 			Field4:       1,
-			Tgt:          tgtHex,
+			Tgt:          hex.EncodeToString(tgt),
 			Field6:       3,
 			ServiceTypes: []int32{1, 5, 10, 21},
 			Field9:       2,
@@ -24,19 +23,10 @@ func BuildHighWayUrlReq(tgt []byte) ([]byte, error) {
 			Field11:      8,
 			Ver:          "1.0.1",
 		},
-	}
-	packet, err := proto.Marshal(body)
-	if err != nil {
-		return nil, err
-	}
-	return packet, nil
+	})
 }
 
-func ParseHighWayUrlReq(data []byte) (*action.HttpConn0X6Ff_501Response, error) {
-	var req action.HttpConn0X6Ff_501Response
-	err := proto.Unmarshal(data, &req)
-	if err != nil {
-		return nil, err
-	}
-	return &req, nil
+func ParseHighWayUrlReq(data []byte) (req action.HttpConn0X6Ff_501Response, err error) {
+	err = proto.Unmarshal(data, &req)
+	return
 }
