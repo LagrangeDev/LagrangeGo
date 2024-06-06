b04c1614133eb31075b445072ebb78638d6cbbb0
Redmomn <109732988+Redmomn@users.noreply.github.com>
2024-06-06 12:10:43 +0800

    docs: 添加googleAnalytics

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,8 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
+import {h} from 'vue'
+import type {Theme} from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import googleAnalytics from 'vitepress-plugin-google-analytics'
 import './style.css'
 
 export default {
@@ -11,7 +12,9 @@ export default {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
-  enhanceApp({ app, router, siteData }) {
-    // ...
+  enhanceApp({app, router, siteData}) {
+    googleAnalytics({
+      id: 'G-BLBVX50DXQ'
+    })
   }
 } satisfies Theme
