7e3ce8889a1141aa3095f9dafa4b86cb1b0b14d4
Redmomn <109732988+Redmomn@users.noreply.github.com>
2024-07-24 21:08:47 +0800

    feat: 处理加群请求

--- client/operation.go
+++ client/operation.go
@@ -337,3 +337,16 @@ func (c *QQClient) GetGroupSystemMessages(groupUin ...uint32) ([]*entity.GroupJo
 	}
 	return oidb2.ParseFetchGroupSystemMessagesReq(resp, groupUin...)
 }
+
+// SetGroupRequest 处理加群请求
+func (c *QQClient) SetGroupRequest(accept bool, sequence uint64, typ uint32, groupUin uint32, message string) error {
+	pkt, err := oidb2.BuildSetGroupRequestReq(accept, sequence, typ, groupUin, message)
+	if err != nil {
+		return err
+	}
+	resp, err := c.sendOidbPacketAndWait(pkt)
+	if err != nil {
+		return err
+	}
+	return oidb2.ParseSetGroupRequestResp(resp)
+}
