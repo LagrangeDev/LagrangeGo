68e634cd8e62ad385a35e02fcabe3b2a372f69fa
pk5ls20 <pk5ls20@outlook.com>
2024-08-16 03:59:34 +0800

    chore: resolve https://github.com/LagrangeDev/LagrangeGo/pull/71#discussion_r1718243135

--- client/packets/oidb/GroupFSDownload.go
+++ client/packets/oidb/GroupFSDownload.go
@@ -3,12 +3,12 @@ package oidb
 import (
 	"encoding/hex"
 	"fmt"
-	oidb2 "github.com/LagrangeDev/LagrangeGo/client/packets/pb/service/oidb"
+	"github.com/LagrangeDev/LagrangeGo/client/packets/pb/service/oidb"
 )
 
 func BuildGroupFSDownloadReq(groupUin uint32, fileId string) (*OidbPacket, error) {
-	body := &oidb2.OidbSvcTrpcTcp0X6D6{
-		Download: &oidb2.OidbSvcTrpcTcp0X6D6Download{
+	body := &oidb.OidbSvcTrpcTcp0X6D6{
+		Download: &oidb.OidbSvcTrpcTcp0X6D6Download{
 			GroupUin: groupUin,
 			AppId:    7,
 			BusId:    102,
@@ -19,7 +19,7 @@ func BuildGroupFSDownloadReq(groupUin uint32, fileId string) (*OidbPacket, error
 }
 
 func ParseGroupFSDownloadResp(data []byte) (string, error) {
-	var resp oidb2.OidbSvcTrpcTcp0X6D6Response
+	var resp oidb.OidbSvcTrpcTcp0X6D6Response
 	if _, err := ParseOidbPacket(data, &resp); err != nil {
 		return "", err
 	}
