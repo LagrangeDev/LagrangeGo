4078ca9863a75179debe017d71eb4047c6f83d92
Redmomn <109732988+Redmomn@users.noreply.github.com>
2024-08-26 14:50:01 +0800

    fix: 修复可能的空指针引用

--- client/packets/oidb/FetchGroupSystemMessages.go
+++ client/packets/oidb/FetchGroupSystemMessages.go
@@ -3,7 +3,6 @@ package oidb
 import (
 	"github.com/LagrangeDev/LagrangeGo/client/entity"
 	"github.com/LagrangeDev/LagrangeGo/client/packets/pb/service/oidb"
-	"github.com/LagrangeDev/LagrangeGo/utils"
 )
 
 func BuildFetchGroupSystemMessagesReq(count uint32) (*OidbPacket, error) {
@@ -24,16 +23,21 @@ func ParseFetchGroupSystemMessagesReq(data []byte, groupUin ...uint32) ([]*entit
 		if len(groupUin) > 0 && groupUin[0] != r.Group.GroupUin {
 			continue
 		}
-		requests = append(requests, &entity.GroupJoinRequest{
-			GroupUin:    r.Group.GroupUin,
-			TargetUid:   r.Target.Uid,
-			Sequence:    r.Sequence,
-			State:       entity.EventState(r.State),
-			EventType:   r.EventType,
-			Comment:     r.Comment,
-			InvitorUid:  utils.Ternary[string](r.Invitor != nil, r.Invitor.Uid, ""),
-			OperatorUid: utils.Ternary[string](r.Operator != nil, r.Operator.Uid, ""),
-		})
+		req := &entity.GroupJoinRequest{
+			GroupUin:  r.Group.GroupUin,
+			TargetUid: r.Target.Uid,
+			Sequence:  r.Sequence,
+			State:     entity.EventState(r.State),
+			EventType: r.EventType,
+			Comment:   r.Comment,
+		}
+		if r.Invitor != nil {
+			req.InvitorUid = r.Invitor.Uid
+		}
+		if r.Operator != nil {
+			req.OperatorUid = r.Operator.Uid
+		}
+		requests = append(requests, req)
 	}
 	return requests, nil
 }
