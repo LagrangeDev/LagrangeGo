4262624c16dca1fd98c2d137dd7db34c57cc7eab
pk5ls20 <pk5ls20@outlook.com>
2024-09-29 10:42:13 +0800

    feat: Add more http api (#97)
    * feat: Add more http api
    - reference: https://github.com/Mrs4s/MiraiGo/blob/master/client/http_api.go
    
    * fix: FetchEssenceMessage
    
    * feat: split `SendRequestWithCookie` into two functions

---
+++ client/entity/group_notice.go
@@ -0,0 +1,35 @@
+package entity
+
+// reference https://github.com/Mrs4s/MiraiGo/blob/master/client/http_api.go
+type (
+	GroupNoticeRsp struct {
+		Feeds []*GroupNoticeFeed `json:"feeds"`
+		Inst  []*GroupNoticeFeed `json:"inst"`
+	}
+
+	GroupNoticeFeed struct {
+		NoticeId    string `json:"fid"`
+		SenderId    uint32 `json:"u"`
+		PublishTime uint64 `json:"pubt"`
+		Message     struct {
+			Text   string        `json:"text"`
+			Images []NoticeImage `json:"pics"`
+		} `json:"msg"`
+	}
+
+	NoticePicUpResponse struct {
+		ErrorCode    int    `json:"ec"`
+		ErrorMessage string `json:"em"`
+		ID           string `json:"id"`
+	}
+
+	NoticeImage struct {
+		Height string `json:"h"`
+		Width  string `json:"w"`
+		ID     string `json:"id"`
+	}
+
+	NoticeSendResp struct {
+		NoticeId string `json:"new_fid"`
+	}
+)
