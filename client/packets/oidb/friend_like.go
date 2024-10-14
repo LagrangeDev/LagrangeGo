9b6c02deff42fa2111b9166d776d2557a29f6f6c
icarus-ai <82353054+icarus-ai@users.noreply.github.com>
2024-10-14 09:36:47 +0800

    add: SendFriendLike 发送好友赞

--- client/packets/oidb/friend_like.go
+++ client/packets/oidb/friend_like.go
@@ -8,8 +8,8 @@ import (
 func BuildFriendLikeReq(uid string, count uint32) (*OidbPacket, error) {
 	body := &oidb.OidbSvcTrpcTcp0X7E5_104{
 		TargetUid: proto.Some(uid),
-		Field2:    71,
-		Field3:    count,
+		Source   : 71,
+		Count    : count,
 	}
 	return BuildOidbPacket(0x7E5, 104, body, false, false)
 }
